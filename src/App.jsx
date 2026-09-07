7b32342378dac045daf6eee8ccecc5fc7b6d9ca6
Riyadb023 <riyadbenyamina32@gmail.com>
2026-09-07 18:52:35 +0100

    finished phase 1 of the project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Dream Car Builder</h1>
+      <p>Welcome to your garage where you build your dream car!</p>
+      <button>Start Building</button>
+    </div>
+  );
+}
+
+export default App;
